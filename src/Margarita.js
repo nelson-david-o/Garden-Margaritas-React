6ead01b5e78f311c830b2d225d37e66984d41189
_devchr1s <polidavis28@gmail.com>
2023-09-21 15:30:13 -0500

    add sunflowers and random flojers

--- src/Margarita.js
+++ src/Margarita.js
@@ -1,8 +1,9 @@
 // Margarita.js
 
-import React, { useState } from 'react';
-import './Margarita.css';
-import margaritaImage from './assets/margarita.png';
+import React, { useState } from "react";
+import "./Margarita.css";
+import margaritaImage from "./assets/margarita.png";
+import girasolImage from "./assets/girasol.png";
 
 const Margarita = () => {
   const [blooms, setBlooms] = useState([]);
@@ -10,17 +11,30 @@ const Margarita = () => {
   const handleClick = (e) => {
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
-    setBlooms([...blooms, { x, y }]);
+    const img = _getTypeFlower();
+    setBlooms([...blooms, { x, y, image: img }]);
+  };
+
+  const _getTypeFlower = () => {
+    const number = Math.round(Math.random());
+    let img;
+    if (number > 0) {
+      img = girasolImage;
+    } else {
+      img = margaritaImage;
+    }
+
+    return img;
   };
 
   return (
     <div className="margarita-container">
-      <h1>JARDIN DE MARGARITAS</h1>
+      <h1>JARDIN DE FLORES</h1>
       <div className="margarita-garden" onClick={handleClick}>
         {blooms.map((bloom, index) => (
           <img
             key={index}
-            src={margaritaImage}
+            src={bloom.image}
             alt="Margarita"
             className="margarita-bloom"
             style={{ top: `${bloom.y}px`, left: `${bloom.x}px` }}
